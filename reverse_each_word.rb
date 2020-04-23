87fda1aeed0ca1a79afbcab47fdbbddaaff7849c
Ashlen Mencia <ashmencia0103@gmail.com>
2020-04-23 21:19:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
 def reverse_each_word(sentence)
-  sentence_array = sentence.split(" ")
+  reverse_array = sentence.split(" ")
   # I want you to turn my sentence argument into an array (.split)
   new_array = []
   # this is where I am storing that reversed array
-  sentence_array.collect {|word| word.reverse }
+  reverse_array.collect {|word| word.reverse }
         # I want to collect each word and reverse it and put it in my "new array"
   new_array.join(" ")
       # we are joining the new array into one big old family aka. one string
